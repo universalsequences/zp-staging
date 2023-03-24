e3e921370b89b4e63ff3ee90a9c00bcac24d5821
Alec Resende <universalsequences@gmail.com>
2023-03-24 14:22:11 -0400

    staging

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a3c7df8bc5a5878515142f228abe6ff.js"
+  "/precache-manifest.7e8ab9de42bb70cc8d37ed379c9c1b33.js"
 );
 
 workbox.clientsClaim();
